231649932815201ed75e029d3318f2770a991b99
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2023-07-11 13:55:26 +0530

    add init

---
+++ public/projects/redpitaya-server/Readme.md
@@ -0,0 +1,11 @@
+## RedPitaya LocalServer
+### Install
+```sh
+# only Sanic is needed
+pip3 install -r requirements.txt
+```
+
+### Run
+```sh
+python3 ./index.py
+```
--- public/projects/redpitaya-server/index.py
+++ public/projects/redpitaya-server/index.py
@@ -1,8 +1,23 @@
 from sanic import Sanic
-from sanic.response import text, json
+from sanic.response import (
+  text, json, html, file_stream
+);
 
-app = Sanic("redpitaya")
+PORT=1337;
 
-@app.get("/")
+app = Sanic("redpitaya");
+
+app.static("/", "./index.html");
+app.static("/assets", "./assets", name="assets");
+
+@app.get("/get")
 async def index(request):
-    return text("Hello, world.")
+    return html("<h1>RedPitaya Server</h1>");
+
+if __name__ == "__main__":
+  print(f"Starting RedPitaya Server on port {PORT}");
+  app.run(
+    host='0.0.0.0',
+    port=PORT,
+    access_log=False
+  );
