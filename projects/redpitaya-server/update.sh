cef5113b19ed9aec59909bcc54c83d97f91c6436
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2023-07-25 13:40:09 +0530

    add updater

---
+++ projects/redpitaya-server/update.sh
@@ -0,0 +1,6 @@
+cd /webapps/
+rm -rf /webapps/cquicc /webapps/server
+git clone https://github.com/plutoniumm/cquicc.git
+
+# Get file out
+mv /webapps/cquicc/projects/redpitaya-server /webapps/server
