d4e12a96a1a2b54f137954da50dfa32753023fc4
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2023-09-01 04:29:43 +0530

    Add memory to slides

--- src/routes/present/utils.js
+++ src/routes/present/utils.js
@@ -4,19 +4,18 @@ import template from "./render.html?raw"
 
 const mcf = { mangle: false, headerIds: false };
 export const render = ( text ) => {
-  text = text.replaceAll( "===", "\n</section><section>\n" )
-
   const [ , meta, ...rest ] = text.split( "---" );
   const m = yaml.load( meta.trim(), { json: true } );
 
-  let html = "<section>\n" +
-    marked( rest.join( "---" ), mcf ) +
-    "\n</section>";
+  let html = rest
+    .join( "---" )
+    .split( "===" )
+    .map( section => marked( section, mcf ) )
+    .join( "</section><section>" );
 
-  console.log( html );
   html = template
     .replace( "&head;", "" )
-    .replace( "&body;", html );
+    .replace( "&body;", `<section>${ html }</section>` );
 
   for ( let key in m )
     html = html.replace( `&${ key };`, m[ key ] );
---
+++ static/presents/ltim.md
@@ -0,0 +1,29 @@
+---
+topic: "QSVM for LTIM at MCQuICC"
+---
+
+### Received Dataset
+
+|Coolant T|Fuel P|LOG(Engine rpm)|LOG(Lub oil P)|LOG(Coolant P)|lub oil T|Engine Condition|
+|---|---|---|---|---|---|---|
+|-0.7727|0.3442|0.4828|-0.2438|0.2430|0.5634|1|
+|...|...|...|...|...|...|...|
+|...|...|...|1200 rows|...|...|
+|...|...|...|...|...|...|...|
+|-0.0863|-0.0622|-0.3165|-0.2442|0.5177|0.0347|1|
+
+<div class="tl">
+
+- T=Temp,P=Pressure
+- 6 features `[-1,1]`
+- 1 label `{0,1}`
+
+</div>
+
+===
+
+PAGE TWO
+
+===
+
+PAGE tres
